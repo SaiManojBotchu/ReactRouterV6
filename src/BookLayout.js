e4772a7cc0060f4dc829347103ce3192c237677c
Sai Manoj <saimanoj2826@gmail.com>
2023-02-07 21:31:45 +0530

    Learned about useSearchParams

--- src/BookLayout.js
+++ src/BookLayout.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet, useSearchParams } from 'react-router-dom';
 
 function BookLayout() {
+  const [searchParams, setSearchParams] = useSearchParams({ n: 3 });
+  const number = searchParams.get('n');
   return (
     <>
-      <nav>
-        <NavLink activeclasssname='active' to='/books/1'>Book1</NavLink>
+      <nav className='App-Nav'>
+        <NavLink activeclasssname='active' to='/books/1'>
+          Book1
+        </NavLink>
         <br />
-        <NavLink activeclasssname='active' to='/books/2'>Book2</NavLink>
+        <NavLink activeclasssname='active' to='/books/2'>
+          Book2
+        </NavLink>
         <br />
-        <NavLink activeclasssname='active' to='/books/new'>NewBook</NavLink>
+        <NavLink activeclasssname='active' to={`/books/${number}`}>
+          Book{number}
+        </NavLink>
+        <br />
+        <NavLink activeclasssname='active' to='/books/new'>
+          NewBook
+        </NavLink>
       </nav>
       {/* The <Outlet> renders the current route selected. */}
       <Outlet />
+      <input
+        type='number'
+        value={number}
+        onChange={(e) => setSearchParams({ n: e.target.value })}
+      />
     </>
   );
 }
